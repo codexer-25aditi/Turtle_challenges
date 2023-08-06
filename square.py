23123e56f244e419fe5ce83771988ef9b9c84d06
ADITI SAHU <88385979+codexer-25aditi@users.noreply.github.com>
2023-08-06 22:49:58 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,15 @@
+from turtle import Turtle,Screen
+
+timmy=Turtle()
+screen=Screen()
+timmy.shape("circle")
+timmy.color("red")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+screen.exitonclick()
